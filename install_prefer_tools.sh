5a0323ead525de80b512b1fd727a914832bea6b9
Arton Chang <arton0306@gmail.com>
2012-11-03 11:51:15 -0700

    add some tools

--- install_prefer_tools.sh
+++ install_prefer_tools.sh
@@ -7,7 +7,8 @@ git config --global core.editor "vim"
 # tools ( the alias are set in ~/.bashrc )
 update
 install -y vim
-install -y curl
 install -y autojump
 install -y aptitude
 install -y screen
+install -y meld # a diff tool ( http://meldmerge.org/ )
+install -y gitg # git gui client tool
