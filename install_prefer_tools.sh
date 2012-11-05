f2d0861213f1701beb2b1d1eb9787f3a651128f9
Arton Chang <arton0306@gmail.com>
2012-11-05 17:40:15 +0800

    add some gui tools

--- install_prefer_tools.sh
+++ install_prefer_tools.sh
@@ -4,12 +4,16 @@ git config --global user.email arton0306@gmail.com
 git config --global color.ui true
 git config --global core.editor "vim"
 
-# tools ( the alias are set in ~/.bashrc )
+# terminal tools ( the alias are set in ~/.bashrc )
 update
 install -y vim
 install -y tcsh
 install -y autojump
 install -y aptitude
 install -y screen
+
+# gui tools
+install -y synaptic
 install -y meld # a diff tool ( http://meldmerge.org/ )
 install -y gitg # git gui client tool
+install -y git-gui
