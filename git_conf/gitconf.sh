a5df9b53d054d551e50a355320b8966d5e77ed8e
Arton Chang <arton0306@gmail.com>
2012-12-27 15:39:49 +0800

    add git global ignore

--- git_conf/gitconf.sh
+++ git_conf/gitconf.sh
@@ -1,7 +1,11 @@
+if ! [ -f $HOME/.gitignore_global ]; then
+    cp .gitignore_global ~
+fi
+
 # set a little configuration
 git config --global user.name "Arton Chang"
 git config --global user.email arton0306@gmail.com
 git config --global color.ui true
 git config --global core.editor "vim"
 git config --global alias.tree "log --graph --decorate --pretty=oneline --abbrev-commit --all"
-
+git config --global core.excludesfile ~/.gitignore_global
