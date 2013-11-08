bf5663ffd7a3200968bb17c7e22142ff7ceee6cc
Arton Chang <arton@zillians.com>
2013-11-08 16:08:48 +0800

    add link .bash_profile for mac

--- create_conf_file_link.sh
+++ create_conf_file_link.sh
@@ -11,3 +11,15 @@ ln -s ~/Configure/.screenrc ~/.screenrc
 # vim
 ln -s ~/Configure/vim/.vimrc ~/.vimrc
 ln -s ~/Configure/vim ~/.vim
+
+# for mac
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Darwin' ]]; then
+    platform='freebsd'
+    if [ -f $HOME/.bash_profile ]; then
+        echo ".bash_profile exists"
+    else
+        ln -s ~/.bashrc ~/.bash_profile
+    fi
+fi
