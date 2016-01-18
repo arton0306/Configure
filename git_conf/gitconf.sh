eee4027ddf8d9e242851a6a39e150e6605b8e8b5
Arton Chang <arton0306@gmail.com>
2016-01-17 19:28:55 -0800

    update git conf

--- git_conf/gitconf.sh
+++ git_conf/gitconf.sh
@@ -7,5 +7,5 @@ git config --global user.name "Arton Chang"
 git config --global user.email arton0306@gmail.com
 git config --global color.ui true
 git config --global core.editor "vim"
-git config --global alias.tree "log --graph --decorate --pretty=oneline --abbrev-commit --all"
 git config --global core.excludesfile ~/.gitignore_global
+git config --global alias.tree "log --graph --decorate --pretty=oneline --abbrev-commit --all"
