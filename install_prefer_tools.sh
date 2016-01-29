bd40b02d62c92e342c76b770a39aa535d368f559
Arton Chang <arton0306@gmail.com>
2016-01-29 19:33:01 +0800

    update prefer tools

--- install_prefer_tools.sh
+++ install_prefer_tools.sh
@@ -1,32 +1,43 @@
-# terminal tools ( the alias are set in ~/.bashrc )
 update
+
+#--------------------------------
+# Must install
+#--------------------------------
 install -y vim
 install -y autojump
 install -y tmux
-install -y htop     # a tool more convenient than top
-install -y ruby
-install -y rubygems # ruby install packeage
 install -y apt-file
-install -y dtrx     # do the right extract
-install -y cmake-curses-gui
+install -y dtrx             # do the right extract
+install -y cmake-curses-gui # ccmake command
+install -y ruby
+install -y rubygems         # ruby package manager
 
+#--------------------------------
+# GUI env tools
+#--------------------------------
+# install -y synaptic       # gui for apt-get
+# install -y meld           # a diff tool ( http://meldmerge.org/ )
+# install -y gitg           # git gui client tool
+# install -y git-gui
+
+#--------------------------------
+# sometime used tools
+#--------------------------------
+# install -y htop           # a tool more convenient than top
 # install -y tcsh
-# install -y aptitude # a text apt-get tool
-# install -y screen
 # install -y openvpn
 # install -y ctags
-# install -y exuberant-ctags
-# install -y byobu    # a convineint tool which backend is screen
-# install -y mosh     # a remote terminal application ( http://mosh.mit.edu/ )
-# install -y openssh-server
-# install -y procps   # for top
-
-# gui tools
-# install -y synaptic # a gui apt-get tool
-# install -y meld # a diff tool ( http://meldmerge.org/ )
-# install -y gitg # git gui client tool
-# install -y git-gui
 # install -y cgdb
+# install -y screen
+
+#--------------------------------
+# Bak (just memorize the name)
+#--------------------------------
+# install -y aptitude       # a text apt-get tool
+# install -y byobu          # a convineint tool which backend is screen
+# install -y mosh           # a remote terminal application ( http://mosh.mit.edu/ )
+# install -y exuberant-ctags
+# install -y openssh-server
 
 # unetbooting dependency
 # install -y ia32-libs
