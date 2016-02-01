78c0a8e3ef0ee64244a8a05469e612eaba130cdd
Arton Chang <arton0306@gmail.com>
2016-01-31 19:50:01 -0700

    update new_computer_setup.sh

--- new_computer_setup.sh
+++ new_computer_setup.sh
@@ -2,6 +2,8 @@ sudo apt-get install -y git
 sudo apt-get install -y autojump
 sudo apt-get install -y tmux
 sudo apt-get install -y vim
+sudo apt-get install -y dtrx
+sudo apt-get install -y apt-file
 
 if [ -f $HOME/.bashrc ]; then
     mv $HOME/.bashrc $HOME/.bashrc.orig
