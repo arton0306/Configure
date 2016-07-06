197a79fae29e1a2335e48b85db030eba4a007252
Arton Chang <arton0306@gmail.com>
2016-07-06 02:10:13 -0700

    add  testshell

---
+++ util/testshell.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "argc=" << argc << endl;
+    for (int i = 0; i < argc; ++i) {
+        cout << "[" << argv[i] << "]" << endl;
+    }
+    return 0;
+}
