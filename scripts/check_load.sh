6c51f742624f9638bf066266b972384265b0f3b0
Arton Chang <arton0306@gmail.com>
2018-08-10 22:31:42 -0700

    fix check_load.sh

--- scripts/check_load.sh
+++ scripts/check_load.sh
@@ -2,21 +2,27 @@ run_task () {
     echo 'routine runs!'
 }
 
-low_load_counter=0
+low_load_min=0
+wait_min=0
 while true; do
     load=$(uptime | awk '{print $(NF-2)}' | awk -F . '{print $1}')
-    if [ $(echo "$load < 1" | bc) ]; then
-        echo "ok:) load=$load"
-        low_load_counter=$low_load_counter+1
-        if (($low_load_counter>=10)); then
+    is_low_load=$(echo "$load < 2" | bc)
+    if (($is_low_load)); then
+        echo "$HOST load=$load, low load for $low_load_min mins"
+        if (($low_load_min>=10)); then
             ps aux > ps.snapshot
             echo "$HOST load is low for 10min. Time to kick off the task!"
             run_task
             break
         fi
     else
-        low_load_counter=0
-        echo "wait... $HOST load=$load"
+        low_load_min=0
+        echo "$wait_min mins passed. Please wait... $HOST load=$load"
     fi
+
     sleep 1
+    ((wait_min=$wait_min+1))
+    if [ $is_low_load ]; then
+        ((low_load_min=$low_load_min+1))
+    fi
 done
