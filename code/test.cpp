a33d81125221078bfa5fbda6a880d6807320dcb4
Arton Chang <arton0306@gmail.com>
2019-09-25 20:24:03 +0800

    remove code

---
+++ code/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) {
+    std::cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+    const char *comma = strchr(names + 1, ',');
+    std::cerr.write(names, comma - names) << ": " << arg1 << " |";
+    __f(comma + 1, args...);
+}
+
+int main() {
+    int a = 5;
+    double pi = 3.14;
+    trace(a,pi);
+    return 0;
+}
