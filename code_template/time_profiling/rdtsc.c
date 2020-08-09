9644b7f27f7262db9e58416e3866af91b8a17a4d
arton <arton0306@gmail.com>
2020-08-10 01:16:06 +0800

    check rdtsc vs clock_gettime in sleep(1)

--- code_template/time_profiling/rdtsc.c
+++ code_template/time_profiling/rdtsc.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 
 int main()
 {
-        unsigned long long val1;
-        unsigned long long val2;
+    unsigned long long val1;
+    unsigned long long val2;
 
-        val1 = __builtin_ia32_rdtsc();
-        sleep(1);
-        val2 = __builtin_ia32_rdtsc();
+    struct timespec t1,t2;
+    clock_gettime(CLOCK_REALTIME, &t1);
+    val1 = __builtin_ia32_rdtsc();
+    sleep(1);
+    val2 = __builtin_ia32_rdtsc();
+    clock_gettime(CLOCK_REALTIME, &t2);
 
-        printf ("ts1: %lld\n", (val1));
-        printf ("ts2: %lld\n", (val2));
-        printf ("diff: %lld\n", val2-val1);
+    printf ("ts1: %lld\n", (val1));
+    printf ("ts2: %lld\n", (val2));
+    printf ("diff in Hz: %lld\n", val2-val1);
+    printf ("diff in sec: %lf\n", (val2-val1)/3.4);
+    printf ("diff clock_gettime in sec: %ld\n",
+            (t2.tv_sec - t1.tv_sec) * 1000000000 +
+            t2.tv_nsec - t1.tv_nsec);
 
-        return (0);
+    return 0;
 }
-
