7614b74f28a253a5ee0bb22fbda03b86db8ae513
arton <arton0306@gmail.com>
2020-08-29 14:13:50 +0800

    add more type in show_basic_size.cpp

--- util/show_basic_size.cpp
+++ util/show_basic_size.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <map>
 #include <unordered_map>
@@ -8,6 +9,7 @@
 using namespace std;
 
 int main() {
+    cout << "char: " << sizeof(char) << endl;
     cout << "short: " << sizeof(short) << endl;
     cout << "int: " << sizeof(int) << endl;
     cout << "long: " << sizeof(long) << endl;
@@ -22,8 +24,15 @@ int main() {
     cout << "set<int,int>: " << sizeof(set<int,int>) << endl;
     cout << "unordered_set<int,int>: " << sizeof(unordered_set<int,int>) << endl;
     cout << "-----" << endl;
+    cout << "string: " << sizeof(string) << endl;
+    cout << "string_view: " << sizeof(string_view) << endl;
+    cout << "-----" << endl;
     cout << "unique_ptr<int>: " << sizeof(unique_ptr<int>) << endl;
     cout << "shared_ptr<int>: " << sizeof(shared_ptr<int>) << endl;
+    cout << "weak_ptr<int>: " << sizeof(weak_ptr<int>) << endl;
+    cout << "-----" << endl;
+    cout << "sturct timespec: " << sizeof(struct timespec) << endl;
+    cout << "chrono::high_resolution_clock::time_point: " << sizeof(chrono::high_resolution_clock::time_point) << endl;
 
     return 0;
 }
